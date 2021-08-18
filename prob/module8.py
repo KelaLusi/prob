2139923c9f03ec3ff838657a343b1c72d2406692
Лукерья <keralusi@gmail.com>
2021-08-19 00:13:18 +0300

    8 done

---
+++ prob/module8.py
@@ -0,0 +1,30 @@
+import os
+
+path = 'C:\\Users\\lusha\\PycharmProjects\\prob\\prob'
+file1 = 'text.txt'
+file2 = 'analysis.txt'
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+alpha_dict = {}
+percent_alpha_dict = {}
+text_len = 0
+
+with open(os.path.join(path, file1), 'r') as text:
+    for iline in text:
+        for icher in iline:
+            icher = icher.lower()
+            if icher in alpha_dict:
+                alpha_dict[icher] += 1
+                text_len += 1
+            elif icher == '\n' or icher == ' ':
+                continue
+            else:
+                alpha_dict[icher] = 1
+                text_len += 1
+
+with open(os.path.join(path, file2), 'w') as text2:
+    for key, value in alpha_dict.items():
+        text2.write(key + ' ' + str(round((value / text_len), 3)) + '\n')
+        # percent_alpha_dict[key] = round((value / text_len), 3)
+
+with open(os.path.join(path, file2), 'r') as text2:
+    print(text2.read())
